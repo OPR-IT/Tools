91ec420909ef967db77f1988c6526210f94bb3c6
OPR-IT <153236572+OPR-IT@users.noreply.github.com>
2023-12-12 13:36:19 -0800

    Update WSL-HyperV-IPBridge.ps1

--- Network/WSL-HyperV-IPBridge.ps1
+++ Network/WSL-HyperV-IPBridge.ps1
@@ -1,1 +1,2 @@
+# For enabling communication between WSL and VMs.
 Get-NetIPInterface | where {$_.InterfaceAlias -eq 'vEthernet (WSL)' -or $_.InterfaceAlias -eq 'vEthernet (Default Switch)'} | Set-NetIPInterface -Forwarding Enabled
