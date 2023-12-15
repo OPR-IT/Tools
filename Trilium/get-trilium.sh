06b832eea32203c0d642c29cdb08a0d5631eb2d9
OPR-IT <153236572+OPR-IT@users.noreply.github.com>
2023-12-15 15:42:30 -0800

    Update get-trilium.sh

--- Trilium/get-trilium.sh
+++ Trilium/get-trilium.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 if [ $1 -z ]; then
-	V='60.4'
+        V=$(curl -I https://github.com/zadam/trilium/releases/latest | grep 'location: *' | cut -d\/ -f8 | cut -d\. -f2- | sed -r s/\\r//)
 else
-	V=$1
+        V=$1
 fi
-bigname="trilium-linux-x64-server-0.${V}.tar.xz"
-url="https://github.com/zadam/trilium/releases/download/v0.${V}/${bigname}"
-if [ -f $bigname ]; then
-	rm -v $bigname
+name="trilium-linux-x64-server-0.${V}.tar.xz"
+url="https://github.com/zadam/trilium/releases/download/v0.${V}/${name}"
+if [ -f $name ]; then
+        rm -v $name
 fi
-wget $url -P ~/ && tar -xvf $bigname
-rm -v $bigname
+wget --show-progress -O $name $url && tar -xvf $name
+rm -v $name*
+if [ -d trilium-server ]; then
+        rm -rv trilium-server
+fi
+mv -fv trilium-linux-x64-server trilium-server && \
+        cd trilium-server
