8ed21f85308527ab09e8da318f3066a23e89e244
oprsf <153236572+oprsf@users.noreply.github.com>
2023-12-22 12:23:38 -0800

    Update install-and-run-trilium-on-docker.sh

--- Trilium/install-and-run-trilium-on-docker.sh
+++ Trilium/install-and-run-trilium-on-docker.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+if [ -z $1 ]; then
+	$_1='8080'
+else
+	$_1="$1"
+fi
+if [ -z $2 ]; then
+	if [ -d 'trilium-data' ]; then
+		$_2='trilium-data1'
+	else
+		$_2='trilium-data'
+	fi
+else
+	$_2="$2"
+fi
 sudo docker pull zadam/trilium && \
-sudo docker run --rm -dtip 0.0.0.0:8080:8080 -v ~/trilium-data:/home/node/trilium-data zadam/trilium
+sudo docker run --rm -dtip 0.0.0.0:$_1:$_1 -v ~/"$_2":/home/node/trilium-data zadam/trilium
