b35182c5f0cf924790b0e30276245396efa52bf4
oprsf <153236572+oprsf@users.noreply.github.com>
2023-12-22 14:13:51 -0800

    Delete Wordpress directory

--- Wordpress/enable-wordpress.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo a2ensite wordpress.conf
-sudo a2enmod rewrite
-sudo a2dissite 000-default.conf
-sudo systemctl restart apache2
--- Wordpress/get-wordpress.sh
+++
@@ -1,6 +0,0 @@
-if [ -f latest.zip ]; then
-	rm latest.zip
-fi
-wget https://wordpress.org/latest.zip
-unzip latest.zip
-rm latest.zip
