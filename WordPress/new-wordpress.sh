22e2d2e626bf79fd798ec81b24dacc603f83390c
oprsf <153236572+oprsf@users.noreply.github.com>
2023-12-22 14:40:06 -0800

    Update new-wordpress.sh

--- WordPress/new-wordpress.sh
+++ WordPress/new-wordpress.sh
@@ -9,8 +9,12 @@ read -s -p "Enter the password for the database user: " DB_PASSWORD
 echo ""
 read -p "Enter your website URL (e.g., http://yourdomain.com): " WP_URL
 
-# Download WordPress
+# Create WordPress installation directory
+sudo mkdir -p $WP_DIR
+sudo chown -R $USER:$USER $WP_DIR
 cd $WP_DIR
+
+# Download WordPress
 wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 rm latest.tar.gz
@@ -29,7 +33,6 @@ mysql -u root -p -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost
 mysql -u root -p -e "FLUSH PRIVILEGES"
 
 # Set up WordPress site
-cd $WP_DIR/wordpress
 mv * ../
 cd ..
 rmdir wordpress
@@ -48,31 +51,31 @@ sudo certbot certonly --webroot -w $WP_DIR -d $DOMAIN_NAME
 # Create Apache Virtual Host Configuration File
 sudo tee /etc/apache2/sites-available/$DOMAIN_NAME.conf > /dev/null << EOF
 <VirtualHost *:80>
-	ServerAdmin webmaster@$DOMAIN_NAME
-	ServerName $DOMAIN_NAME
-	DocumentRoot $WP_DIR
-	ErrorLog \${APACHE_LOG_DIR}/error.log
-	CustomLog \${APACHE_LOG_DIR}/access.log combined
-	RewriteEngine On
-	RewriteCond %{HTTPS} off
-	RewriteRule ^ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+    ServerAdmin webmaster@$DOMAIN_NAME
+    ServerName $DOMAIN_NAME
+    DocumentRoot $WP_DIR
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
+    RewriteEngine On
+    RewriteCond %{HTTPS} off
+    RewriteRule ^ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
 </VirtualHost>
 
 <VirtualHost *:443>
-	ServerAdmin webmaster@$DOMAIN_NAME
-	ServerName $DOMAIN_NAME
-	DocumentRoot $WP_DIR
-	ErrorLog \${APACHE_LOG_DIR}/error.log
-	CustomLog \${APACHE_LOG_DIR}/access.log combined
-	<Directory $WP_DIR>
-		Options FollowSymLinks
-		AllowOverride All
-		Require all granted
-	</Directory>
-	SSLEngine on
-	SSLCertificateFile /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem
-	SSLCertificateKeyFile /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem
-	Include /etc/letsencrypt/options-ssl-apache.conf
+    ServerAdmin webmaster@$DOMAIN_NAME
+    ServerName $DOMAIN_NAME
+    DocumentRoot $WP_DIR
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
+    <Directory $WP_DIR>
+        Options FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+    SSLEngine on
+    SSLCertificateFile /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem
+    SSLCertificateKeyFile /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem
+    Include /etc/letsencrypt/options-ssl-apache.conf
 </VirtualHost>
 EOF
 
@@ -81,4 +84,4 @@ sudo a2ensite $DOMAIN_NAME.conf
 sudo a2enmod ssl
 sudo systemctl restart apache2
 
-echo "WordPress installed successfully with Apache virtual host, Certbot-generated SSL, and HTTP to HTTPS redirection!"
+echo "WordPress installed successfully with directories created, Apache virtual host, Certbot-generated SSL, and HTTP to HTTPS redirection!"
