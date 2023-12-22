659f6c6f7919a612a643faecd01ff79770816ccb
oprsf <153236572+oprsf@users.noreply.github.com>
2023-12-22 12:41:34 -0800

    Update install-and-run-trilium-on-docker.sh

--- Trilium/install-and-run-trilium-on-docker.sh
+++ Trilium/install-and-run-trilium-on-docker.sh
@@ -1,16 +1,16 @@
 if [ -z $1 ]; then
-	$_1="8080"
+	_1='8080'
 else
-	$_1="$1"
+	_1="$1"
 fi
 if [ -z $2 ]; then
 	if [ -d 'trilium-data' ]; then
-		$_2="trilium-data1"
+		_2='trilium-data1'
 	else
-		$_2="trilium-data"
+		_2='trilium-data'
 	fi
 else
-	$_2="$2"
+	_2="$2"
 fi
 sudo docker pull zadam/trilium && \
 sudo docker run --rm -dtip 0.0.0.0:"$_1":"$_1" -v ~/"$_2":/home/node/trilium-data zadam/trilium
