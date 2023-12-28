ca3bf0ba484372dbcf5be473dd164bb9a37b1fcc
oprsf <153236572+oprsf@users.noreply.github.com>
2023-12-28 09:00:50 -0800

    DSFDSF

--- WordPress/newpress8.sh
+++ WordPress/newpress8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Derive variables from a domain
+# Derive Variables from Domain
 read -p "Enter your new domain (e.g., yourdomain.com): " DOMAIN_NAME
 WP_URL="https://${DOMAIN_NAME}"
 SUB_DIR="${DOMAIN_NAME//./}"
@@ -9,7 +9,7 @@ DB_NAME="${SUB_DIR}_db"
 DB_USER="${SUB_DIR}_user"
 DB_PASSWORD="${SUB_DIR}_pass"
 
-# Create WordPress directory
+# Create WordPress Directory
 mkdir -p $WP_DIR
 chown -R www-data:www-data $WP_DIR
 chmod -R 755 $WP_DIR
@@ -20,7 +20,7 @@ wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 rm latest.tar.gz
 
-# Extract contents here
+# Extract Contents Here
 mv wordpress/* ./
 rm -r wordpress
 
@@ -30,20 +30,20 @@ sed -i "s/database_name_here/$DB_NAME/" $WP_DIR/wp-config.php
 sed -i "s/username_here/$DB_USER/" $WP_DIR/wp-config.php
 sed -i "s/password_here/$DB_PASSWORD/" $WP_DIR/wp-config.php
 
-# Set up the database
+# Make Database
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -u root -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-# Update site URL in WordPress database
+# Update Database
 # mysql -u $DB_USER -p$DB_PASSWORD -e "USE $DB_NAME; UPDATE wp_options SET option_value='$WP_URL' # WHERE option_name='siteurl';"
 # mysql -u $DB_USER -p$DB_PASSWORD -e "USE $DB_NAME; UPDATE wp_options SET option_value='$WP_URL' # WHERE option_name='home';"
 
-# Generate CSR with Certbot
+# Make CSR to Let's Encrypt
 certbot certonly --webroot -w $WP_DIR -d $DOMAIN_NAME
 
-# Create Apache Virtual Host Configuration File
+# Make Apache Config
 tee /etc/apache2/sites-available/$SUB_DIR.conf > /dev/null << EOF
 <VirtualHost *:80>
 
@@ -54,39 +54,35 @@ tee /etc/apache2/sites-available/$SUB_DIR.conf > /dev/null << EOF
 
 <IfModule mod_ssl.c>
 	<VirtualHost *:443>
- 		ServerAdmin webmaster@${DOMAIN_NAME}
-   		ServerName ${DOMAIN_NAME}
-	 	DocumentRoot ${WP_DIR}
-   
-   		ErrorLog \${APACHE_LOG_DIR}/error.log
-	 	CustomLog \${APACHE_LOG_DIR}/access.log combined
- 	
-  	<Directory ${WP_DIR}>
- 		Options FollowSymLinks
- 		AllowOverride All
- 		Require all granted
- 	</Directory>
- 		
-   		SSLEngine on
-  		SSLCertificateFile /etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem
-  		SSLCertificateKeyFile /etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem
+		ServerAdmin webmaster@${DOMAIN_NAME}
+		ServerName ${DOMAIN_NAME}
+		DocumentRoot ${WP_DIR}
 
- 	</VirtualHost>
+		ErrorLog \${APACHE_LOG_DIR}/error.log
+		CustomLog \${APACHE_LOG_DIR}/access.log combined
+	
+	<Directory ${WP_DIR}>
+		Options FollowSymLinks
+		AllowOverride All
+		Require all granted
+	</Directory>
+		
+		SSLEngine on
+		SSLCertificateFile /etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem
+		SSLCertificateKeyFile /etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem
+
+	</VirtualHost>
 </IfModule>
 EOF
 
-# Enable Apache Virtual Host and SSL
+# Enable Site & SSL
 sudo a2ensite $SUB_DIR.conf
 sudo a2enmod ssl
 sudo systemctl reload apache2
 
-# Make CSR to Let's Encrypt
-certbot certonly --webroot -d ${DOMAIN_NAME} -w ${WP_DIR}
-
-# Print installation summary
+# Print Summary
 echo -e "\n  ${WP_URL}\n"
 echo "  Web Directory: ${WP_DIR}"
 echo "  Database Name: ${DB_NAME}"
 echo "  Database User: ${DB_USER}"
 echo -e "  Database Pass: ${DB_PASSWORD}\n"
-
